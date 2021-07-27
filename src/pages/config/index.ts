97164f987d8aa1b67b9832e3f5477325908c9125
inck <kui_cui@ziguangcn.com>
2021-07-27 15:28:54 +0800

    feat: 添加rxjs

--- src/pages/config/index.ts
+++ src/pages/config/index.ts
@@ -1,11 +1,13 @@
 import Home from '../home/config';
+import Main from '../main/config';
 
 const global = {
-  
+
 }
 
 const routeVariable = {
   "/index.html" : Home,
+  "/src/pages/main/index.html": Main
 }
 
 export function pagesVariableConfig(path: string) {
